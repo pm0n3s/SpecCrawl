a1959a2f0f4b203ec9065a25aa0ced8dfdea7869
pm0n3s <patrick.bodin777@gmail.com>
2017-07-06 15:46:18 -0400

    added found txt file

--- spec.py
+++ spec.py
@@ -1,7 +1,7 @@
 #  -*- coding: utf-8 -*-
 
 import PyPDF2, os, pyperclip
-
+key = ['sloan' ]
 # input PDF name to program
 
 pdf = input('What is the pdf name?')
@@ -16,13 +16,18 @@ pdfFile = open(pdf + ".pdf", 'rb')
 
 reader = PyPDF2.PdfFileReader(pdfFile)
 
+newTxt = open(directory + pdf + '.txt', 'a')
+foundTxt = open(directory + 'FOUND' + pdf + '.txt', 'a')
+
 for pageNum in range(reader.numPages):
     txtWall = reader.getPage(pageNum).extractText().encode("utf-8")
-    newTxt = open(directory + pdf + '.txt', 'a+')
     newTxt.write(str(txtWall))
-
-newTxt.close()
-
+    for i in range(len(key)-1):
+        if key[i] in txtWall.lower():
+            foundTxt.write(key[i] + 'found in page ' + pageNum + "/n")
+ 
+newTxt.close()            
+foundTxt.close()
 # search over the txt file for key words 
 
 # create a found txt file 
