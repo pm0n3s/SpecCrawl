6e50b2d0e3298362f27e9470cf02752e1fa0d1b9
pm0n3s <patrick.bodin777@gmail.com>
2017-07-13 23:26:41 -0400

    updated spec file location

--- spec.py
+++ spec.py
@@ -1,42 +1,29 @@
 #  -*- coding: utf-8 -*-
 
 import PyPDF2, os, pyperclip
-key = ['Watts', 'watts', 'Zurn' ]
+key = ['Watts', 'watts', 'Zurn', '' ]
 # input PDF name to program
 
 pdf = input('What is the pdf name?')
 
-directory = 'C:\\ProjectTxtFiles\\'
+directory = 'C:\\Projects\\SpecCrawl\\'
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-os.chdir('C:\\Users\\pbodin\\Downloads')
-# os.chdir('C:\\Users\\Patrick\\Downloads')
+# os.chdir('C:\\Users\\pbodin\\Downloads')
+os.chdir('C:\\Users\\Patrick\\Downloads')
 
 pdfFile = open(pdf + ".pdf", 'rb')
 
 reader = PyPDF2.PdfFileReader(pdfFile)
 
-newTxt = open(directory + pdf + '.txt', 'w')
+newTxt = open(directory + 'testtxt', 'w')
 
 for pageNum in range(reader.numPages):
     txtWall = reader.getPage(pageNum).extractText().encode("utf-8")
     newTxt.write(str(txtWall))
  
 newTxt.close() 
-
-foundTxt = open(directory + 'FOUND' + pdf + '.txt', 'w')
-
-for i in range(len(key)):
-    searchFile = open(directory + pdf + '.txt', 'r')
-    for line in searchFile:
-        if key[i] in line:
-            foundTxt.write(key[i] + ' found\n')
-        else:
-            foundTxt.write(key[i] + ' not found\n')
-    searchFile.close()
-    
-foundTxt.close()
 # search over the txt file for key words 
 
 # create a found txt file 
