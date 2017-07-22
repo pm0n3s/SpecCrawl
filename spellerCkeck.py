eb58dc269dd8a1ec44e9337965a7b549dbb12243
pm0n3s <patrick.bodin777@gmail.com>
2017-07-21 21:58:45 -0400

    commented out/only for ref

--- spellerCkeck.py
+++ spellerCkeck.py
@@ -1,60 +1,60 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import re
-import sys
-from dictionary import Dictionary
+# import re
+# import sys
+# from dictionary import Dictionary
 
-LENGTH = 15
+# LENGTH = 15
 
-dictionary = "C:\\Projects\\SpecCrawl\\keywords"
+# dictionary = "C:\\Projects\\SpecCrawl\\keywords"
 
-d = Dictionary()
-loaded = d.load(dictionary)
+# d = Dictionary()
+# loaded = d.load(dictionary)
 
-if not loaded: 
-    print("Could not load $dictionary.")
-    exit(1)
+# if not loaded: 
+#     print("Could not load $dictionary.")
+#     exit(1)
 
-file = "C:\\Projects\\SpecCrawl\\testtxt"
-fp = open(file, "r", encoding="utf-8")
-if not fp:
-    print("Could not open {}.".format(file))
-    exit(1)
+# file = "C:\\Projects\\SpecCrawl\\testtxt"
+# fp = open(file, "r", encoding="utf-8")
+# if not fp:
+#     print("Could not open {}.".format(file))
+#     exit(1)
 
-print("\nFOUND WORDS\n")
-word = ""
-index, findings, words = 0, 0, 0
+# print("\nFOUND WORDS\n")
+# word = ""
+# index, findings, words = 0, 0, 0
 
-while True:
-    c = fp.read(1)
-    if not c:
-        break
-    if re.match(r'[\S]', c):
-        word += c
-        index += 1
-        if index > LENGTH:
-            while True:
-                c = fp.read(1)
-                if not c or not re.match(r'[\S]', c):
-                    break
-            index, word = 0, ""
-        elif index > 0:
-            found = d.check(word)
-            if found:
-                print(word)
-                findings += 1
-    elif re.match(r'[\s]', c):
-            words += 1
-            index, word = 0, ""
-fp.close()
+# while True:
+#     c = fp.read(1)
+#     if not c:
+#         break
+#     if re.match(r'[\S]', c):
+#         word += c
+#         index += 1
+#         if index > LENGTH:
+#             while True:
+#                 c = fp.read(1)
+#                 if not c or not re.match(r'[\S]', c):
+#                     break
+#             index, word = 0, ""
+#         elif index > 0:
+#             found = d.check(word)
+#             if found:
+#                 print(word)
+#                 findings += 1
+#     elif re.match(r'[\s]', c):
+#             words += 1
+#             index, word = 0, ""
+# fp.close()
 
-n = d.size()
-unloaded = d.unload()
+# n = d.size()
+# unloaded = d.unload()
 
-if not unloaded:
-    print("Could not load $dictionary.")
-    exit(1)
+# if not unloaded:
+#     print("Could not load $dictionary.")
+#     exit(1)
 
-print("\nWORDS FOUND:     {}".format(findings))
-print("WORDS IN DICTIONARY:  {}".format(n))
-print("WORDS IN TEXT:        {}".format(words))
+# print("\nWORDS FOUND:     {}".format(findings))
+# print("WORDS IN DICTIONARY:  {}".format(n))
+# print("WORDS IN TEXT:        {}".format(words))
